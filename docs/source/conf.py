fb04c28104a4feec3736f10ba3327e820e68e053
Dirk Siebelts <DSiebelts@googlemail.com>
2018-06-12 16:47:25 +0200

    Updated conf.py for intersphinx and style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,7 @@ sys.path.insert(0, os.path.abspath('../../mor'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages']
 
@@ -91,7 +92,18 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -157,3 +169,11 @@ texinfo_documents = [
 
 
 
+# -- Options for intersphinx ---------------------------------------------
+
+intersphinx_mapping = {
+    'firedrake': ('https://firedrakeproject.org/', None),
+    'python':('https://docs.python.org/3/', None),
+    'numpy':('https://docs.scipy.org/doc/numpy-1.14.0/',None),
+    'scipy':('https://docs.scipy.org/doc/scipy/reference/',None),
+}
